21ad39c684c300eda007f12aec57a8c36f8f01cf
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2022-10-27 15:12:51 +0200

    add sandbox_bucket2

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,8 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "sandbox_bucket2" {
+  bucket = "aft-sandbox2-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
