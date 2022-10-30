7181603a0472ae59f1ec78cfa14e9dbf2867f258
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2022-10-30 17:12:49 +0100

    add third bucket

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -9,3 +9,8 @@ resource "aws_s3_bucket" "sandbox_bucket2" {
   bucket = "aft-sandbox2-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "sandbox_bucket3" {
+  bucket = "aft-sandbox3-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
