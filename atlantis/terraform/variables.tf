197933f731b49778d919fb233a5435783ac63491
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2022-11-08 16:57:01 +0100

    update

--- atlantis/terraform/variables.tf
+++ atlantis/terraform/variables.tf
@@ -5,12 +5,12 @@ variable "role_name" {
 
 variable "atlantis_aws_account_id" {
   type    = string
-  default = "" 
+  default = "249466986931"
 }
 
 variable "atlantis_iam_role_name" {
   type    = string
-  default = "" 
+  default = "s8-atlantis"
 }
 
 variable "atlantis_iam_prod_role_name" {
--- sandbox2/terraform/s3.tf
+++
@@ -1,6 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "sandbox_bucket3" {
-  bucket = "aft-sandbox3-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
-}
